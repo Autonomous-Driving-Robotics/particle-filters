c65b0ccd9b59b89acfd05d0b2b7c99f9814db528
Dikshant Gupta <dikshant2210@gmail.com>
2024-01-23 12:08:26 +0000

    basic project setup

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "src/particle-filter/particle_filter.hpp"
+
+int main()
+{
+    return 0;
+}
---
+++ src/particle-filter/particle_filter.hpp
@@ -0,0 +1,13 @@
+#ifndef PARTICLE_FILTER_HPP
+#define PARTICLE_FILTER_HPP
+
+template <typename T>
+class ParticleFilter
+{
+  private:
+    /* data */
+  public:
+    ParticleFilter();
+};
+
+#endif
